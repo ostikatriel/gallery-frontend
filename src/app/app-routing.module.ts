8868a1d525f1e9a5e2039b37644c15b725b93617
Osti Katriel <ostikatriel@gmail.com>
2021-05-04 18:35:05 -0500

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { ListarComponent } from './pages/listar/listar.component';
+import { NuevaComponent } from './pages/nueva/nueva.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListarComponent},
+  {path: 'nueva', component: NuevaComponent},
+  {path: 'detalle', component: DetalleComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListarComponent } from './pages/listar/listar.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { NuevaComponent } from './pages/nueva/nueva.component';
+// external
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    DetalleComponent,
+    NuevaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+
+    NgbModalModule,
+    HttpClientModule,
+    FormsModule
   ],
+  entryComponents:[DetalleComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
